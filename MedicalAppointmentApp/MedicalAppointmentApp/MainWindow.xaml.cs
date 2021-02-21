3ba752930397101efbb9d37786847cafde8023df
SirVexus <varmo.info@gmail.com>
2021-02-21 01:23:31 +0100

    Main Page + Visual Updates
    Added main page with option to switch between pages, application Icon, automatic xml documentation generator

--- MedicalAppointmentApp/MedicalAppointmentApp/MainWindow.xaml.cs
+++ MedicalAppointmentApp/MedicalAppointmentApp/MainWindow.xaml.cs
@@ -1,28 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MedicalAppointmentApp.Pages;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MedicalAppointmentApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
+    /// On Admin Click -> Redirect to Add data Page
+    /// On Doctor Click -> Redirect to List data Page as doctor
+    /// On Customer Click -> Redirect to List data Page as customer
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Main Window Page Constructor (no arguments)
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+        }
+        
+        private void Admin_Click(object sender, RoutedEventArgs e)
+        {
+            AdminPage adminPage = new AdminPage();
+            this.Content = adminPage;
+        }
+
+        private void Doctors_Click(object sender, RoutedEventArgs e)
+        {
+            ListPage listPage = new ListPage(true);
+            this.Content = listPage;
+        }
+
+        private void Customers_Click(object sender, RoutedEventArgs e)
+        {
+            ListPage listPage = new ListPage(false);
+            this.Content = listPage;
         }
     }
 }
